be32beeae1adaad19ad213298dddecb8efaa2e0a
Nguyen Nguyen <n.nguyen3464@gmail.com>
2024-04-04 15:38:05 -0500

    worked on delete functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,52 +1,41 @@
     class RecipesController < ApplicationController
 
-        before_action :authenticate_user!, only: [:update]
+        # before_action :authenticate_user!, only: [:update]
 
         def index
             @recipes = Recipe.all
-            puts @recipes.inspect
             render json: @recipes
         end
     
         def create
             recipe = Recipe.create(recipe_params)
             if recipe.valid?
-                render json: recipe, status: 200
+              render json: recipe
             else 
-                render json: { errors: recipe.errors }, status: 422
+              render json: recipe.errors, status: 422
             end
-        end
+          end
         
         
         def update
             recipe = Recipe.find(params[:id])
-            if recipe.user == current_user  # Ensure only the recipe owner can update it
-              if recipe.update(recipe_params)
-                render json: recipe, status: :ok
-              else
-                render json: { errors: recipe.errors }, status: :unprocessable_entity
-              end
+            recipe.update(recipe_params)
+            if recipe.valid?
+              render json: recipe
             else
-              render json: { error: "You are not authorized to update this recipe" }, status: :unauthorized
+              render json: recipe.errors, status: 422
             end
           end
-        
+          
           def destroy
-            recipe = Recipe.find_by(id: params[:id])
-            if recipe
-              if current_user && recipe.user == current_user  # Ensure current user is authorized to delete the recipe
-                if recipe.destroy
-                  render json: recipe
-                else
-                  render json: { errors: recipe.errors }, status: 422
-                end
-              else
-                render json: { error: "You are not authorized to delete this recipe" }, status: :unauthorized
-              end
-            else
-              render json: { error: "Recipe not found" }, status: 404
+            recipe = Recipe.find(params[:id])
+            if recipe.destroy
+              render json: { message: "Recipe deleted successfully" }, status: 200
+            else 
+              render json: { error: "Failed to delete recipe" }, status: 422
             end
           end
+          
           
           
           
